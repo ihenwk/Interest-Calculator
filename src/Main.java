9d8cd6834349671890256a9cc5b949ae9b363044
Ihesinachi Nwankwo <ihe.nwk@hotmail.co.uk>
2023-04-19 00:10:37 +0100

    fixed earnings and updated values for print statements so they show to two decimal places

--- src/Main.java
+++ src/Main.java
@@ -16,61 +16,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Hello");
+        System.out.println("Hello and welcome to the interest calculator!");
+        System.out.println("");
 
         System.out.println("How much would you like to invest?");
         String userInvestment = scan.nextLine();
-        int initialPrincipleAmount =Integer.parseInt(userInvestment) ; //initial amount of principle
+        int initialPrincipleAmount = Integer.parseInt(userInvestment); //initial amount of principle
 
         System.out.println("How many years would you like to invest for?");
         String numYears = scan.nextLine();
-        int numFundYears = Integer.parseInt(numYears)  ; //number of years money on fund
+        int numFundYears = Integer.parseInt(numYears); //number of years user has chosen to keep money in fund for
 
         System.out.println("What is the annual interest rate % growth?");
         String interestGrowth = scan.nextLine();
         double annualInterestRate = Double.parseDouble(interestGrowth); //annual interest rate
 
-        double quarterlyInterestRate = annualInterestRate/4; //calculates the interest rate for each financial quarter.
+        double quarterlyInterestRate = annualInterestRate / 4; //calculates the interest rate for each financial quarter.
 
-        double currentBalance = initialPrincipleAmount;
+        double currentBalance = initialPrincipleAmount; //current balance uses user's amount they have chosen to invest.
         int i = 0; // counter to keep track of the number of years/loops program has done.
 
-        while (i < numFundYears){
+        while (i < numFundYears) {
+            double yearlyStartBalance = currentBalance; // used to keep track of balance for start of each year
+            System.out.println("Year " + i + ":");
+            System.out.println("Current balance at the start of the year: £" + String.format("%.2f",yearlyStartBalance));
 
-           double balanceForFirstQuarter = currentBalance * (1 + (quarterlyInterestRate / 100)); // calculates amount made in first quarter of the year
-           currentBalance += (balanceForFirstQuarter-currentBalance); //updates current balance variable to be used in the next quarter calculation
+            //Calculations to update current balance for first, second, third and fourth quarters of the year.
+            //Current balance is updated to use the balance from the previous quarter.
+        double balanceForFirstQuarter = currentBalance * (1 + (quarterlyInterestRate / 100)); // calculates amount made in first quarter of the year
+        currentBalance += (balanceForFirstQuarter - currentBalance); //updates current balance variable to be used in the next quarter calculation
 
-           double balanceForSecondQuarter = currentBalance * (1 + (quarterlyInterestRate / 100)); // calculates amount made in second quarter of the year
-           currentBalance += (balanceForSecondQuarter-currentBalance); //updates current balance variable to be used in the next quarter calculation
+        double balanceForSecondQuarter = currentBalance * (1 + (quarterlyInterestRate / 100)); // calculates amount made in second quarter of the year
+        currentBalance += (balanceForSecondQuarter - currentBalance); //updates current balance variable to be used in the next quarter calculation
 
-          double balanceForThirdQuarter = currentBalance * (1 + (quarterlyInterestRate / 100)); // calculates amount made in third quarter of the year
-           currentBalance += (balanceForThirdQuarter-currentBalance); //updates current balance variable to be used in the next quarter calculation
+        double balanceForThirdQuarter = currentBalance * (1 + (quarterlyInterestRate / 100)); // calculates amount made in third quarter of the year
+        currentBalance += (balanceForThirdQuarter - currentBalance); //updates current balance variable to be used in the next quarter calculation
 
-            double balanceForForthQuarter = currentBalance * (1 + (quarterlyInterestRate / 100)); // calculates amount made in fourth quarter of the year
-            currentBalance += (balanceForForthQuarter-currentBalance); //updates current balance variable to be used in the next quarter calculation
+        double balanceForFourthQuarter = currentBalance * (1 + (quarterlyInterestRate / 100)); // calculates amount made in fourth quarter of the year
+        currentBalance += (balanceForFourthQuarter - currentBalance); //updates current balance variable to be used in the next quarter calculation
 
-            System.out.println(" balance for forth quarter: "+ balanceForForthQuarter);
-           double yearlyBalance = currentBalance; // balance at the end of the financial year and will be start balance for the next year
-            System.out.println("yearly balance:" + yearlyBalance);
-           //double finalYearlyStartBalance = (balanceForForthQuarter - yearlyBalance) + yearlyBalance; // calculates the balance at the start of the year
-
-           i+=1; // increases counter that keeps track of the number of loops program has done.
-
-            System.out.println("Year "+ i +":"); //prints out year 1, year 2 etc
-
-            if (i == 1){
-                int yearlyStartBalance = initialPrincipleAmount;
-                System.out.println("Current balance at the start of the year: £" + yearlyStartBalance);
-                double firstYearEarnings = currentBalance - initialPrincipleAmount;
-                System.out.println("You earned: " + "£" + firstYearEarnings );
-            } else {
-                System.out.println("Current balance at the start of the year: £" + currentBalance);
-                double yearlyEarnings = balanceForForthQuarter - yearlyBalance;
-                System.out.println(" You earned: " + "£" + yearlyEarnings);
-            }
-            System.out.println("Ended year with: " +"£" + currentBalance);
+            double yearlyEarnings = currentBalance - yearlyStartBalance;
+            System.out.println("You earned: " + "£" + String.format("%.2f",yearlyEarnings));
+            System.out.println("Ended year with: " + "£" + String.format("%.2f",currentBalance));
             System.out.println("_________________________________");
             System.out.println();
-        } // need to work out how to get the ended with balance and yearly earnings for year 2 and onwards
+
+            i += 1; // increases counter that keeps track of the number of loops program has done.
+
     }
-}
+        }
+    }
